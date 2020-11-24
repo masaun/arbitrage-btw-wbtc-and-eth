cb4d08740d43fbc30ebc2aefc4c7aa432b598694
masaun <masanoriuno@gmail.com>
2020-11-24 22:35:19 +0900

    Add CErc20.sol

--- contracts/compound/interfaces/cToken.sol
+++ contracts/compound/interfaces/cToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-interface cToken {
+interface CToken {
     function mint(uint256 mintAmount) external returns (uint256);
 
     function redeem(uint256 redeemTokens) external returns (uint256);
