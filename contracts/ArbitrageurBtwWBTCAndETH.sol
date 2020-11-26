5c25f8de7c87303494c39e31b642defef6341500
masaun <masanoriuno@gmail.com>
2020-11-26 09:24:44 +0900

    Rename with executeArbitrage()

--- contracts/ArbitrageurBtwWBTCAndETH.sol
+++ contracts/ArbitrageurBtwWBTCAndETH.sol
@@ -43,7 +43,7 @@ contract ArbitrageurBtwWBTCAndETH {
     /***
      * @notice - Executor of flash swap for arbitrage profit (1: by using the flow of buying)
      **/
-    function executeArbitrageByBuying(
+    function executeArbitrage(
         address payable userAddress, 
         uint WBTCAmount,
         address payable _cEther,
